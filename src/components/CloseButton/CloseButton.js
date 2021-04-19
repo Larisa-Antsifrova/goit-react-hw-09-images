e002678aab8e259a1bae5e163bd49ee5cfd33a48
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-19 23:56:50 +0300

    Added icon to button.

--- src/components/CloseButton/CloseButton.js
+++ src/components/CloseButton/CloseButton.js
@@ -1,12 +1,14 @@
 // React imports
 import React from 'react';
+// Icons imports
+import { MdClose } from 'react-icons/md';
 // Styles imports
 import styles from './CloseButton.module.css';
 
 const CloseButton = ({ onClose }) => {
   return (
     <button type="button" className={styles.CloseButton} onClick={onClose}>
-      x
+      <MdClose />
     </button>
   );
 };
