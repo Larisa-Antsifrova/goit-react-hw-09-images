741dcd3fdfd65e76298d4738bfeee817c945a9bf
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-19 23:13:08 +0300

    Refactored axios configuration for fetch request.

--- src/services/pixabayApi.js
+++ src/services/pixabayApi.js
@@ -1,15 +1,15 @@
+// Axios library imports
 import axios from 'axios';
-
+// Configuring Axios
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = 'https://pixabay.com/api/';
 
 // Function to fetch images with query. Returns an array of result objects
-function fetchImages({ query = '', page = 1, perPage = 12 }) {
-  return axios
-    .get(
-      `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
-    )
-    .then(response => response.data.hits);
+async function fetchImages({ query = '', page = 1, perPage = 12 }) {
+  const { data } = await axios.get(
+    `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
+  );
+  return data.hits;
 }
 
 export { fetchImages };
