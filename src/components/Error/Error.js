e59a66d277e19b7d82740913bea4315d38476a08
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-20 00:11:12 +0300

    Refactored comments.

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,6 +1,8 @@
+// React imports
 import React from 'react';
+// Helpers imports
 import PropTypes from 'prop-types';
-
+// Styles imports
 import styles from './Error.module.css';
 
 const Error = ({ message }) => {
