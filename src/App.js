4146183a7bd86dc9d1ec9b4707c31e66e533d7de
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-20 14:37:59 +0300

    Added comments.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import _ from 'lodash';
 
 export default function App() {
+  // Declaring all state slices
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
@@ -25,11 +26,13 @@ export default function App() {
   const [selectedImage, setSelectedImage] = useState('');
   const [showModal, setShowModal] = useState(false);
 
+  // useEffect is used to send fetch request for a new batch of images.
   useEffect(() => {
+    // When the page is loaded for the first time, we do not expect any pictures to be rendered.
     if (!query) return;
-
+    // Setting loading to true to display loader.
     setIsLoading(true);
-
+    // Sending fetch request to get a new batch of images.
     fetchImages({
       query,
       page,
@@ -41,8 +44,11 @@ export default function App() {
         setError(error);
       })
       .finally(() => setIsLoading(false));
+    // Sending fetch request depends on query and page.
+    // If any of the two or both parameters change we want to send new fetch request.
   }, [page, query]);
 
+  // When the new form submited we want to resent all state fields except for the query.
   const handleSubmit = newQuery => {
     setImages([]);
     setQuery(newQuery);
@@ -53,17 +59,21 @@ export default function App() {
     setShowModal(false);
   };
 
+  // Function to set the large image url to be used in Modal, and to open Modal.
   const setLargeImg = largeImageURL => {
     setSelectedImage(largeImageURL);
     toggleModal();
   };
 
+  // Function to increase page in the state. We want to page to be increased only when we press the Load more button.
   const increasePage = () => setPage(prevPage => prevPage + 1);
 
+  // Function to open or close Modal.
   const toggleModal = () => {
     setShowModal(!showModal);
   };
 
+  // Function to delete one image from the displayed batch.
   const deleteImage = imageId => {
     setImages(prevImages => prevImages.filter(image => image.id !== imageId));
   };
